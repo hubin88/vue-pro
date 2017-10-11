8131c58b4ca8b51730ce45c15b39c36a330e8376
hubin_88 <764326904@qq.com>
2017-10-11 09:48:19 +0800

    first

---
+++ login/src/components/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="login">
+    <h3>登录</h3>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px">
+      <el-form-item label="账号" prop="userName">
+        <el-input v-model="ruleForm.userName" placeholder="请输入手机号码"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="ruleForm.password" type="password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data: function () {
+      return {
+        ruleForm:{
+          userName: '',
+          password: '',
+        },
+        rules: {
+          userName: [
+            { required: true, message: '请输入手机号码', trigger: 'blur' },
+            {  min: 11, message: '手机号码为11位数字', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min:6, max: 32, message: '请输入6~32位密码', trigger: 'blur' }
+          ],
+        }
+      }
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    },
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .login {
+    margin: 0 auto;
+    max-width: 500px;
+  }
+</style>
